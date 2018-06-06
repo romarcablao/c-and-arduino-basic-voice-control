bc42ad0ec2f81f7c2eea924ed67f6381ececf5d5
Romar Cablao <romarcablao@gmail.com>
2018-06-06 22:34:11 +0800

    Initial Commit

---
+++ Voice Controller-Color Changer Arduino/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Voice_Controller_Color_Changer_Arduino
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.bntColor = new System.Windows.Forms.Button();
+            this.serialPortArduino = new System.IO.Ports.SerialPort(this.components);
+            this.pnlHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pnlHeader.Controls.Add(this.bntColor);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1000, 550);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // bntColor
+            // 
+            this.bntColor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bntColor.FlatAppearance.BorderSize = 0;
+            this.bntColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntColor.Font = new System.Drawing.Font("Century Gothic", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntColor.ForeColor = System.Drawing.Color.White;
+            this.bntColor.Location = new System.Drawing.Point(0, 0);
+            this.bntColor.Name = "bntColor";
+            this.bntColor.Size = new System.Drawing.Size(1000, 550);
+            this.bntColor.TabIndex = 0;
+            this.bntColor.Text = "COLOR CHANGER";
+            this.bntColor.UseVisualStyleBackColor = true;
+            this.bntColor.MouseDown += new System.Windows.Forms.MouseEventHandler(this.bntColor_MouseDown);
+            this.bntColor.MouseMove += new System.Windows.Forms.MouseEventHandler(this.bntColor_MouseMove);
+            this.bntColor.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bntColor_MouseUp);
+            // 
+            // serialPortArduino
+            // 
+            this.serialPortArduino.PortName = "COM3";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1000, 550);
+            this.Controls.Add(this.pnlHeader);
+            this.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "COLOR CHANGER";
+            this.pnlHeader.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Button bntColor;
+        private System.IO.Ports.SerialPort serialPortArduino;
+    }
+}
+
---
+++ Voice Controller-Color Changer Arduino/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+
+
+
+namespace Voice_Controller_Color_Changer_Arduino
+{
+    public partial class frmMain : Form
+    {
+
+        //Speech Recog Var
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        SpeechRecognizer recognizer = new SpeechRecognizer();
+
+        //Move Panel Var
+        bool move;
+        int mX, mY;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SelectVoiceByHints(VoiceGender.Female);
+
+            try
+            {
+                serialPortArduino.Open();
+            }
+            catch (Exception)
+            {
+                synth.Speak("Not Connected to Arduino!");
+            }
+
+            synth.Speak("This example demonstrates a basic voice control with the use of C# and Arduino.");
+            synth.Speak("Say 'RED', to change L-E-D light to red. Say 'YELLOW', to change L-E-D light to yellow. Say 'GREEN', to change L-E-D light to green.");
+            synth.Speak("Say 'EXIT', to close the application. Let's Start!");
+
+            Listen();
+        }
+
+        private void Listen()
+        {
+            
+            Choices colors = new Choices();
+            colors.Add(new string[] { "red", "yellow", "green","exit" });
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(colors);
+            
+            Grammar g = new Grammar(gb);
+            recognizer.LoadGrammar(g);
+            
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+          
+        }
+
+        private void bntColor_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move == true)
+            {
+                this.Top = Cursor.Position.Y - mY;
+                this.Left = Cursor.Position.X - mX;
+            }
+        }
+
+        private void bntColor_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        private void bntColor_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mX = Cursor.Position.X - this.Left;
+            mY = Cursor.Position.Y - this.Top;
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text.ToUpper() == "RED")
+            {
+                this.bntColor.BackColor = Color.Red;
+                this.bntColor.Text = "RED";
+                synth.Speak("Turning red on!");
+
+                try
+                {
+                    serialPortArduino.Write("red");
+                }
+                catch (Exception)
+                {
+                   // synth.Speak("Not Connected to Arduino!");
+                }
+                
+            }
+            else if (e.Result.Text.ToUpper() == "YELLOW")
+            {
+                this.bntColor.BackColor = Color.Yellow;
+                this.bntColor.Text = "YELLOW";
+                synth.Speak("Turning yellow on!");
+
+                try
+                {
+                    serialPortArduino.Write("yellow");
+                }
+                catch (Exception)
+                {
+                   // synth.Speak("Not Connected to Arduino!");
+                }
+
+            }
+            else if (e.Result.Text.ToUpper() == "GREEN")
+            {
+                this.bntColor.BackColor = Color.Green;
+                this.bntColor.Text = "GREEN";
+                synth.Speak("Turning green on!");
+
+                try
+                {
+                    serialPortArduino.Write("green");
+                }
+                catch (Exception)
+                {
+                   // synth.Speak("Not Connected to Arduino!");
+                }
+
+            }
+            else if(e.Result.Text.ToUpper() == "EXIT")
+            {
+                synth.Speak("Thank you for testing me out! See you! Exiting Application...");
+                recognizer.Dispose();
+                serialPortArduino.Close();
+                this.Close();
+            }
+           
+        }
+
+    }
+}
